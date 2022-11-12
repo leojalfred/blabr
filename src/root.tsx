e0f2ba37a507a7e1c01b5b588ea872a2166ccfba
Leo Alfred <leojalfred@gmail.com>
2022-11-11 20:46:13 -0600

    Remove unused boilerplate

--- src/root.tsx
+++ src/root.tsx
@@ -1,17 +1,14 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
-import { QwikCity, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
-
-import globalStyles from './global.css?inline';
+import { component$, useStyles$ } from '@builder.io/qwik'
+import {
+  QwikCity,
+  RouterOutlet,
+  ServiceWorkerRegister
+} from '@builder.io/qwik-city'
+import { RouterHead } from './components/router-head/router-head'
+import globalStyles from './global.css?inline'
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCity> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
-  useStyles$(globalStyles);
+  useStyles$(globalStyles)
 
   return (
     <QwikCity>
@@ -25,5 +22,5 @@ export default component$(() => {
         <ServiceWorkerRegister />
       </body>
     </QwikCity>
-  );
-});
+  )
+})
