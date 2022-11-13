964fc956b18371edfa6f96d7ea5084de0fa300fa
Leo Alfred <leojalfred@gmail.com>
2022-11-12 20:53:33 -0600

    Add counter

---
+++ src/components/timeline/counter/counter.tsx
@@ -0,0 +1,45 @@
+import { component$, Signal, useStylesScoped$ } from '@builder.io/qwik'
+import styles from './counter.css?inline'
+
+interface Props {
+  count: Signal
+  circle: Signal
+}
+
+export default component$(({ count, circle }: Props) => {
+  useStylesScoped$(styles)
+
+  return (
+    <div class="counter" ref={count} data-percent="0">
+      <svg
+        class="counter__svg"
+        width="20"
+        height="20"
+        viewPort="0 0 10 10"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          class="counter__circle counter__circle--outline"
+          r="9"
+          cx="10"
+          cy="10"
+          fill="transparent"
+          stroke-dasharray="56.548"
+          stroke-dashoffset="0"
+        ></circle>
+        <circle
+          class="counter__circle counter__circle--fill"
+          ref={circle}
+          id="bar"
+          r="9"
+          cx="10"
+          cy="10"
+          fill="transparent"
+          stroke-dasharray="56.548"
+          stroke-dashoffset="0"
+        ></circle>
+      </svg>
+    </div>
+  )
+})
--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -1,23 +1,68 @@
 import {
+  $,
   component$,
   useClientEffect$,
+  useSignal,
   useStylesScoped$
 } from '@builder.io/qwik'
 import feather from 'feather-icons'
+import Counter from './counter/counter'
 import styles from './timeline.css?inline'
 
 export default component$(() => {
   useStylesScoped$(styles)
-  useClientEffect$(() => feather.replace())
+
+  const input = useSignal<HTMLInputElement>()
+  const count = useSignal<HTMLDivElement>()
+  const circle = useSignal<HTMLElement>()
+
+  useClientEffect$(() => {
+    feather.replace()
+  })
+
+  const handleInput = $(() => {
+    let val = input.value?.value.length ?? 0 / 280
+    if (val < 0) val = 0
+    else if (val > 100) {
+      val = 100
+
+      if (circle.value) circle.value.classList.add('counter__circle--red')
+    }
+
+    const r = parseFloat(circle.value?.getAttribute('r') ?? '0')
+    const c = 2 * Math.PI * r
+    const percent = ((100 - val) / 100) * c
+
+    if (circle.value) circle.value.style.strokeDashoffset = percent.toString()
+    count.value?.setAttribute('data-percent', val.toString())
+  })
 
   return (
     <div className="timeline">
       <div className="timeline__header-wrapper">
         <div className="timeline__header">
           <h1 className="timeline__heading">Home</h1>
-          <i data-feather="activity"></i>
+          <i className="timeline__activity" data-feather="activity" />
         </div>
       </div>
+      <div className="timeline__header-spacer" />
+      <form
+        className="timeline__form"
+        preventdefault:submit
+        onSubmit$={() => console.log('blab')}
+      >
+        <input
+          className="timeline__input"
+          ref={input}
+          type="text"
+          placeholder="What's happening?"
+          onInput$={handleInput}
+        />
+        <div className="timeline__bottom-line">
+          <Counter count={count} circle={circle} />
+          <button className="timeline__submit">Blab</button>
+        </div>
+      </form>
     </div>
   )
 })
